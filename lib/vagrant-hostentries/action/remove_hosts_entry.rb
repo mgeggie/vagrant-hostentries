9b58584ae7d18135f40672856fb52d1768f30635
trigren <mike@Michaels-MacBook-Pro.local>
2013-05-24 16:04:38 -0400

    Modified to skip removing hostfile entries for hosts that don't exist due to the Vagrantfile being changed.

--- lib/vagrant-hostentries/action/remove_hosts_entry.rb
+++ lib/vagrant-hostentries/action/remove_hosts_entry.rb
@@ -1,27 +1,28 @@
 module VagrantPlugins
-	module Hostentries
-		module Action
-			class RemoveHostsEntry
+  module Hostentries
+    module Action
+      class RemoveHostsEntry
         def initialize(app, env)
           @app = app
           @logger = Log4r::Logger.new("VagrantPlugins::Hosts")
         end
 
         def call(env)
-        	env[:host].remove_hosts_entry(env[:machine].config.vm.hostname)
+          env[:host].remove_hosts_entry(env[:machine].config.vm.hostname)
           env[:machine].env.active_machines.each do |machine|
-          	if !machine.guest.capability?(:remove_hosts_entry)
-          		@logger.warn "Unsupported machine #{machine.config.name}"
-          		next
-          	end
-          	env[:machine].env.active_machines.each do |m|
-          		m_hostname = m.config.hostname
-          	  machine.guest.capability(:remove_hosts_entry, m_hostname)
-          	end
+            next unless machine.methods.member? :guest
+            if !machine.guest.capability?(:remove_hosts_entry)
+              @logger.warn "Unsupported machine #{machine.config.name}"
+              next
+            end
+            env[:machine].env.active_machines.each do |m|
+              m_hostname = m.config.hostname
+              machine.guest.capability(:remove_hosts_entry, m_hostname)
+            end
           end
           return @app.call(env)
         end
-			end
-		end
-	end
+      end
+    end
+  end
 end
