c8ebd35f322dca3d19b0a63ebaa7362196eb1ea1
trigren <mike@Michaels-MacBook-Pro.local>
2013-05-24 16:20:45 -0400

    Cleaned up tabifications in action.rb and fixed update_hosts_entry to only act on servicable guests.

--- lib/vagrant-hostentries/action.rb
+++ lib/vagrant-hostentries/action.rb
@@ -1,24 +1,24 @@
 module VagrantPlugins
-	module Hostentries
-		module Action
-			autoload :RemoveHostsEntry, 'vagrant-hostentries/action/remove_hosts_entry'
-			autoload :UpdateHostsEntry, 'vagrant-hostentries/action/update_hosts_entry'
+  module Hostentries
+    module Action
+      autoload :RemoveHostsEntry, 'vagrant-hostentries/action/remove_hosts_entry'
+      autoload :UpdateHostsEntry, 'vagrant-hostentries/action/update_hosts_entry'
 
-			class << self
-				def update_hosts_entry
-					#@update_hosts_entry ||= ::
-					Vagrant::Action::Builder.new.tap do |b|
-						b.use VagrantPlugins::Hostentries::Action::UpdateHostsEntry
-			  	end
-				end
+      class << self
+        def update_hosts_entry
+          #@update_hosts_entry ||= ::
+          Vagrant::Action::Builder.new.tap do |b|
+            b.use VagrantPlugins::Hostentries::Action::UpdateHostsEntry
+          end
+        end
 
-				def remove_hosts_entry
-					@remove_hosts_entry ||= ::Vagrant::Action::Builder.new.tap do |b|
-						b.use VagrantPlugins::Hostentries::Action::RemoveHostsEntry
-					end
-				end
-			end
+        def remove_hosts_entry
+          @remove_hosts_entry ||= ::Vagrant::Action::Builder.new.tap do |b|
+            b.use VagrantPlugins::Hostentries::Action::RemoveHostsEntry
+          end
+        end
+      end
 
-		end
-	end
+    end
+  end
 end
--- lib/vagrant-hostentries/action/update_hosts_entry.rb
+++ lib/vagrant-hostentries/action/update_hosts_entry.rb
@@ -1,33 +1,37 @@
 require 'log4r'
 
 module VagrantPlugins
-	module Hostentries
-		module Action
-			class UpdateHostsEntry
+  module Hostentries
+    module Action
+      class UpdateHostsEntry
         def initialize(app, env)
           @app = app
           @logger = Log4r::Logger.new("VagrantPlugins::Hosts")
         end
 
         def call(env)
-        	env[:host].update_hosts_entry(env[:machine].guest.capability(:read_ip_address), env[:machine].config.vm.hostname)
+          # Update the host system's hostfile entry for the booting guest
+          env[:host].update_hosts_entry(env[:machine].guest.capability(:read_ip_address), env[:machine].config.vm.hostname)
+
+          # Update each guest machines' host entry with all the other guest machines' entries
           env[:machine].env.active_machines.each do |machine|
-          	m = env[:machine].env.machine(machine[0], machine[1])
-          	if !m.guest.capability?(:update_hosts_entry)
-          		@logger.warn "Unsupported machine #{machine.config.name}"
-          		next
-          	end
-          	env[:machine].env.active_machines.each do |machine2|
-          		m2 = env[:machine].env.machine(machine2[0], machine2[1])
-          		m2_ip = m2.guest.capability(:read_ip_address)
-          		m2_hostname = m2.config.vm.hostname
-          		env[:ui].info "Adding IP: #{m2_ip} -> Hostname: #{m2_hostname}"
-          	  m.guest.capability(:update_hosts_entry, m2_ip, m2_hostname)
-          	end
+            m = env[:machine].env.machine(machine[0], machine[1])
+            if !m.guest.capability?(:update_hosts_entry) || !m.methods.member? :guest
+              @logger.warn "Unsupported machine #{machine.config.name}"
+              next
+            end
+            env[:machine].env.active_machines.each do |machine2|
+              m2 = env[:machine].env.machine(machine2[0], machine2[1])
+              next unless m2.methods.member? :guest # skip machines not in service
+              m2_ip = m2.guest.capability(:read_ip_address)
+              m2_hostname = m2.config.vm.hostname
+              env[:ui].info "Adding IP: #{m2_ip} -> Hostname: #{m2_hostname}"
+              m.guest.capability(:update_hosts_entry, m2_ip, m2_hostname)
+            end
           end
           return @app.call(env)
         end
-			end
-		end
-	end
+      end
+    end
+  end
 end
